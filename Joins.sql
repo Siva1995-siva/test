d84fb3c5be48f16b431a90b9fd007836ad3efd9d
siva <vasanthansiva10@gmail.com>
2023-05-20 00:29:51 +0530

    added newly

--- Joins.sql
+++ Joins.sql
@@ -4,10 +4,3 @@ select t1.Student_id, t1.Student_name,t1.Student_subject, t2.student_professer f
 join studentprof t2 on
 t1.student_subject = t2.student_subject
 order by student_id desc
-
-select t1.Student_id, t1.Student_name,t1.Student_subject, t2.student_professer from studentdata t1
-join studentprof t2 on
-t1.student_subject = t2.student_subject
-order by student_id asc
-
-
